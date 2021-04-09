1a306d6a65bae1a2134b435b8ddd4dd52803c225
pjmai <49274320+pjmai@users.noreply.github.com>
2021-04-09 11:45:25 +0200

    Update RCSwitch.h
    8 syncs + 40 bits

--- RCSwitch.h
+++ RCSwitch.h
@@ -58,7 +58,7 @@
 
 // Number of maximum high/Low changes per packet.
 // We can handle up to (unsigned long) => 32 bit * 2 H/L changes per bit + 2 for sync
-#define RCSWITCH_MAX_CHANGES 67
+#define RCSWITCH_MAX_CHANGES 96
 
 class RCSwitch {
 
